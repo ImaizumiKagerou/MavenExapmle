303c3aa56ce0fbd8fe6f8f8a7e0d0e99dc19ad5a
IK <csqcsq11@163.com>
2022-06-10 15:25:10 +0800

    init

---
+++ src/main/java/org/ik/App.java
@@ -0,0 +1,13 @@
+package org.ik;
+
+/**
+ * @author ik
+ * 2022/6/10-14:26
+ **/
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("App.main");
+    }
+
+}
